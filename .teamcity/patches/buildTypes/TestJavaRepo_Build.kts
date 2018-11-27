811ff33097cf766ef771c26e2b0f4ef0db98e472
admin <admin@TeamCity>
2018-11-27 18:30:10 +0000

    TeamCity change in 'Test Java Repo' project: 1 build step(s) were added to 'Build' build configuration

--- .teamcity/patches/buildTypes/TestJavaRepo_Build.kts
+++ .teamcity/patches/buildTypes/TestJavaRepo_Build.kts
@@ -2,6 +2,7 @@ package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2018_1.*
 import jetbrains.buildServer.configs.kotlin.v2018_1.BuildType
+import jetbrains.buildServer.configs.kotlin.v2018_1.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.v2018_1.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2018_1.ui.*
 
@@ -16,6 +17,14 @@ create(RelativeId("TestJavaRepo"), BuildType({
 
     vcs {
         root(RelativeId("TestJavaRepo_HttpsGithubComPaksvTestJavaRepoGitRefsHeadsMaster"))
+    }
+
+    steps {
+        maven {
+            goals = "clean test"
+            runnerArgs = "-Dmaven.test.failure.ignore=true"
+            mavenVersion = defaultProvidedVersion()
+        }
     }
 
     triggers {
